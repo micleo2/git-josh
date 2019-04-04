f4775a295df9af0a27c17a3cf80948738563ff8c
Michael Leon <mal7@illinois.edu>
2019-04-03 23:35:15 -0500

    Silence curl output and make sure git josh is run from somewhere it can reclone

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo curl https://raw.githubusercontent.com/micleo2/git-josh/master/git-josh > /usr/local/bin/git-josh
+sudo curl -s https://raw.githubusercontent.com/micleo2/git-josh/master/git-josh > /usr/local/bin/git-josh
 sudo chmod +x /usr/local/bin/git-josh
